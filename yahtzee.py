cc9090c866df60b85935058879411901513b8db8
김광진 <kjink.kim@samsung.com>
2014-10-24 13:47:01 +0900

    rest api 구조 작성

--- yahtzee.py
+++ yahtzee.py
@@ -6,5 +6,21 @@ app = Flask(__name__,static_url_path='')
 def yahtzee():
   return "hello world"
 
+@app.route("/enroll")
+def enroll():
+  return "enroll "
+
+@app.route("/players")
+def playerss():
+  return "players"
+
+@app.route("/<user>/roll",methods=['POST'])
+def userRoll(user):
+  return "roll"
+
+@app.route("/<user>/decision",methods=['POST'])
+def userDecision(user):
+  return "decision"
+
 if __name__ == "__main__":
   app.run(debug=True,host='0.0.0.0')
