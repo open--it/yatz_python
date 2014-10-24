eae9c568778f94d96ee6a8aa085e774646d554cf
김광진 <kjink.kim@samsung.com>
2014-10-24 13:50:22 +0900

    decision 구현 and 구현 완료

--- yahtzee.py
+++ yahtzee.py
@@ -52,9 +52,33 @@ def userRoll(user):
     players[id].turn +=1
   return Response(json.dumps(cubes), mimetype='application/json')
 
+smstraight = [ [1,2,3,4], [2,3,4,5], [3,4,5,6] ]
+lgstraight = [ [1,2,3,4,5], [2,3,4,5,6] ]
+rule = [
+ lambda dices : collections.Counter(dices)[1]*1   ## Aces - 0
+,lambda dices : collections.Counter(dices)[2]*2   ## Twos - 1
+,lambda dices : collections.Counter(dices)[3]*3   ## Threes - 2
+,lambda dices : collections.Counter(dices)[4]*4   ## Fours - 3
+,lambda dices : collections.Counter(dices)[5]*5   ## Fives - 4
+,lambda dices : collections.Counter(dices)[6]*6   ## Sixes - 5
+,lambda dices : reduce(lambda x,y:x+y,dices) if len([(x,y)for x,y in collections.Counter(dices).most_common(1) if y >= 3 ]) >= 1 else 0   ## 3 of a kind - 6
+,lambda dices : reduce(lambda x,y:x+y,dices) if len([(x,y)for x,y in collections.Counter(dices).most_common(1) if y >= 4 ]) >= 1 else 0   ## 4 of a kind - 7
+,lambda dices : 25 if reduce(lambda x,y: x+y ,[y for x,y in collections.Counter(dices).most_common(2) if y >= 2]) == 5 else 0   ## Full House - 8
+,lambda dices : 30 if len( [list(x) for x in [set(dices).intersection(set(d)) for d in smstraight] if len(x)==4] ) >0 else 0   ## 30 Sm.Straight - 9
+,lambda dices : 40 if len( [list(x) for x in [set(dices).intersection(set(d)) for d in lgstraight] if len(x)==5] ) >0 else 0   ## 40 Lg.Straight - 10
+,lambda dices : 50 if collections.Counter(dices).values()[0]==5 else 0   ## Yahtzee - 11
+,lambda dices : reduce(lambda x,y:x+y ,dices)   ## chance - 12
+]
 @app.route("/<user>/decision",methods=['POST'])
 def userDecision(user):
-  return "decision"
+  id = int(user)
+  slot = int(request.json["slot"])
+  dices = [int(x["eye"])+1 for x in request.json["dices"] ]
+  point = rule[slot](dices)
+  global players
+  players[id].game[slot] = point
+  players[id].turn = 0
+  return jsonify(slot=slot,point=point)
 
 if __name__ == "__main__":
   app.run(debug=True,host='0.0.0.0')
